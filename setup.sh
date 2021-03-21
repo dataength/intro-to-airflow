6806e1006c13e12ea3df9f316b0dfba0b2f0ee08
zkan <zkan.cs@gmail.com>
2021-03-21 15:23:11 +0700

    Add postgres as extra package :bear:

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@
 AIRFLOW_VERSION=2.0.1
 PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow[postgres]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
